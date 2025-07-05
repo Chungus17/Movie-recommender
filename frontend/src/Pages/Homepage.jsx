e3fb40d5e76abb2b27b728a8c115ee060bc597cc
Haroon Khan <mrharoonkhan11@gmail.com>
2025-07-05 16:48:59 +0300

    Latest-commit (48:49)

--- frontend/src/Pages/Homepage.jsx
+++ frontend/src/Pages/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from "../components/Hero"
+import CardList from "../components/CardList"
 
 const Homepage = () => {
   return (
     <div className='p-5'>
         <Hero />
+        <CardList />
     </div>
   )
 }
--- frontend/src/components/CardList.jsx
+++ frontend/src/components/CardList.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import cardImg from "../assets/cardimg.jpg"
 
 const CardList = () => {
+
+  const data = [
+    {
+        id: 1,
+        title: "card1",
+        subtitle: "This is card1",
+        image: "kjdnksn",
+    },
+    {
+        id: 2,
+        title: "card2",
+        subtitle: "This is card2",
+        image: "kjdnksn",
+    },
+    {
+        id: 3,
+        title: "card3",
+        subtitle: "This is card3",
+        image: "kjdnksn",
+    },
+  ];
+
   return (
-    <div>CardList</div>
+    <div className='text-white md:px-4'>
+        <h2 className='pt-10 pb-5 text-lg font-medium'>Upcoming</h2>
+
+        <div>
+            {data.map((item) => (
+                <div>
+                    <img src={ cardImg } alt="Image" />
+                    <p>{ item.title }</p>
+                </div>
+            ))}
+        </div>
+    </div>
+
+    
   )
 }
 
